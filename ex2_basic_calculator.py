5f87c2d9c715cc53641e98e6d80bed75f3af6ae1
batanai777gw <batanai178@gmail.com>
2024-05-30 12:36:07 +0200

    type_conversion_of_variables

---
+++ ex2_basic_calculator.py
@@ -0,0 +1,14 @@
+# Basic Calculator Program
+# We have to enter two numbers (whole nums and decimal point nums)
+# Print the sum of these two numbers
+
+first = input("Enter the first number: ")
+second = input("Enter the second number: ")
+sum = float(first) + float(second)
+print("Sum: " + str(sum))
+
+# example 2 of writing the same code using type conversion
+# first = float(input("Enter the first number: "))
+# second = float(input("Enter the second number: "))
+# sum = first + second
+# print("Sum: " + str(sum))
